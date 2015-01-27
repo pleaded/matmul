f9ca49df982fbfc0969f61a3a93ed70222ec2e56
pleaded <vzelenkovskiy@yandex.ru>
2015-01-27 23:07:36 +0300

    Done "with_pitched_shared" 1) Добавить выбор блока (16\32 - размер) 2) Добавить ивенты, отслеживать время 3) Оформить все в одну кучу, выбор режимов (??)

--- with_pitched_shared.c
+++ with_pitched_shared.c
@@ -9,7 +9,14 @@
 
 #define get_elem(array, Row, Column) (((float*)((char*)array.ptr + (Row)*array.pitch))[(Column)])
 #define set_elem(array, Row, Column, elem) \
-(((type*)((char*)array.ptr + (Row)*array.pitch))[(Column)] = elem)
+(((float*)((char*)array.ptr + (Row)*array.pitch))[(Column)] = elem)
+
+#define type_get_elem(array, Row, Column) \
+(((float*)((char*)array.ptr + (Row)*array.pitch))[(Column)])
+
+#define type_set_elem(array, Row, Column, elem) \
+(((float*)((char*)array.ptr + (Row)*array.pitch))[(Column)] = elem)
+
 
 __device__ float GetElement(float * A, int row, int col, int stride)
 {
@@ -39,7 +46,43 @@ __global__ void MatMulKernel(cudaPitchedPtr A, cudaPitchedPtr B, cudaPitchedPtr 
 	get_elem(C, row, col) = Sum;
 }
 
-__global__ void MatMulKernelShared(float * A, float * B, float * C, int m, int n, int k)
+__global__
+void kernel_shared(cudaPitchedPtr l_in, cudaPitchedPtr r_in, cudaPitchedPtr out, int n, int m, int k, int block_h) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    const int gy = gridDim.y;
+    if (row >= n || col >= k ){
+        return;
+    }
+    
+    float sum1, sum2 = 0.0;
+    extern __shared__  float ptr[];
+    float *AS = ptr;
+    float *BS = ptr + sizeof(float) * (block_h * SHARED_W);
+    
+    int aBegin = m * BLOCK_SIZE_H * blockIdx.y;
+    int aEnd = aBegin + m - 1;
+    int bBegin = m * BLOCK_SIZE_W * blockIdx.x;
+    int aStep = BLOCK_SIZE_H * m;
+    int bStep = BLOCK_SIZE_W * k;
+    
+    
+    sum1 = 0.0;
+    for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
+        AS[threadIdx.y * BLOCK_SIZE_W + threadIdx.x] = type_get_elem(l_in, threadIdx.y, a + threadIdx.x);
+        BS[threadIdx.y * BLOCK_SIZE_W + threadIdx.x] = type_get_elem(r_in, threadIdx.y, b + threadIdx.x);
+        __syncthreads();
+        for (int k = 0; k < BLOCK_SIZE_H; k++) {
+            sum1 += AS[threadIdx.y * BLOCK_SIZE_H + k] * BS[k * BLOCK_SIZE_H + threadIdx.x];
+        }
+        __syncthreads();
+    }
+
+
+    type_set_elem(out, row, col, sum1);
+}
+
+__global__ void MatMulKernelShared(float * A, float * B, float * C, int n, int m, int k)
 {
 	/*
 	         B  x
@@ -71,16 +114,16 @@ __global__ void MatMulKernelShared(float * A, float * B, float * C, int m, int n
 	float * Csub = GetSubMatrix(C, blockRow, blockCol, k);
 	float Sum = 0.0;
 
-	for(int subCnt = 0; subCnt < (n / BLOCK_SIZE); subCnt++) {
+	for(int subCnt = 0; subCnt < (m / BLOCK_SIZE); subCnt++) {
 		// Asub - fixed row for each thread
 		// Bsub - fixed column for each thread
-		float * Asub = GetSubMatrix(A, blockRow, subCnt, n);
+		float * Asub = GetSubMatrix(A, blockRow, subCnt, m);
 		float * Bsub = GetSubMatrix(B, subCnt, blockCol, k);
 
 		__shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
 		__shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
 
-		As[row][col] = GetElement(Asub, row, col, n);
+		As[row][col] = GetElement(Asub, row, col, m);
 		Bs[row][col] = GetElement(Bsub, row, col, k);
 
 		__syncthreads();
@@ -103,15 +146,15 @@ __global__ void MatMulKernelShared(float * A, float * B, float * C, int m, int n
 
 
 __global__
-void kernel_pitch(cudaPitchedPtr l_in, cudaPitchedPtr r_in, cudaPitchedPtr out, int n, int m, int k) {
+void kernel_pitch(cudaPitchedPtr l_in, cudaPitchedPtr r_in, cudaPitchedPtr out, int m, int n, int k) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row >= n || col >= k ){
+    if (row >= m || col >= k ){
         return;
     }
     
     float sum = 0.0;
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < n; i++) {
         
         sum += type_get_elem(l_in, row, i) * type_get_elem(r_in, i, col);
     }
@@ -127,12 +170,49 @@ void info(cudaPitchedPtr A)
 
 /* 
      nnnn    kkkk    kkkk
-    m####   n####   m####
-	m#### x n#### = m####
-	m####   n####   m####
+    n####   m####   n####
+	n#### x m#### = n####
+	n####   m####   n####
 */
 
-void MatMul(cudaPitchedPtr A, cudaPitchedPtr B, cudaPitchedPtr C, int m, int n, int k)
+cudaPitchedPtr cuda_zmalloc(int m, int n) {
+    cudaPitchedPtr res;
+    res.ysize = ((m - 1) / 32 + 1) * 32;
+    res.pitch = ((n - 1) / 32 + 1) * 32 * sizeof(float);
+    res.xsize = n;
+    cudaMalloc(&(res.ptr), res.pitch * res.ysize);
+    cudaMemset(res.ptr, 0 , res.ysize * res.pitch);
+    
+    return res;
+}
+
+
+void launch_kernel_shared(cudaPitchedPtr A, cudaPitchedPtr B, cudaPitchedPtr C, int m, int n, int k, int block_h)
+{
+    cudaPitchedPtr dA, dB, dC;
+    dA = cuda_zmalloc(m , n);
+    dB = cuda_zmalloc(n, k);
+    dC = cuda_zmalloc(m, k);
+    
+    cudaMemcpy2D(dA.ptr, dA.pitch, A.ptr, n * sizeof(float), n * sizeof(float), m, cudaMemcpyHostToDevice);
+    cudaMemcpy2D(dB.ptr, dB.pitch, B.ptr, k * sizeof(float), k * sizeof(float), n, cudaMemcpyHostToDevice);
+    
+    dim3 threadBlock(BLOCK_SIZE_W, BLOCK_SIZE_H);
+    int bw = (m - 1) / BLOCK_SIZE_W + 1;
+    int bh = (k - 1) / BLOCK_SIZE_H + 1;
+    dim3 grid(bw, bh);
+    
+    kernel_shared<<<grid, threadBlock, 2 * block_h * BLOCK_SIZE_W * sizeof(float) >>>(dA, dB, dC, m, n, k, block_h);
+    
+    
+    cudaFree(dA.ptr);
+    cudaFree(dB.ptr);
+    
+    cudaMemcpy2D(C.ptr, k * sizeof(float), dC.ptr, dC.pitch, k * sizeof(float), m, cudaMemcpyDeviceToHost);
+    cudaFree(dC.ptr);
+}
+
+void MatMul(cudaPitchedPtr A, cudaPitchedPtr B, cudaPitchedPtr C, int n, int m, int k)
 {
 	cudaError_t err;
 	cudaEvent_t start, stop;
@@ -142,16 +222,16 @@ void MatMul(cudaPitchedPtr A, cudaPitchedPtr B, cudaPitchedPtr C, int m, int n, 
 	cudaEventCreate(&stop);	
 	
 	cudaPitchedPtr dA, dB, dC;
-	dA = make_cudaPitchedPtr(0, 0, n, m);
-	dB = make_cudaPitchedPtr(0, 0, k, n);
-	dC = make_cudaPitchedPtr(0, 0, k, m);
+	dA = make_cudaPitchedPtr(0, 0, m, n);
+	dB = make_cudaPitchedPtr(0, 0, k, m);
+	dC = make_cudaPitchedPtr(0, 0, k, n);
 
 	/* dA, dB, dC mem allocation */
-	err = cudaMallocPitch(&dA.ptr, &dA.pitch, n * sizeof(float), m);
+	err = cudaMallocPitch(&dA.ptr, &dA.pitch, m * sizeof(float), n);
 	printf("[MatMul]: cudaMallocPitch A - %s\n", cudaGetErrorString(err));
-	err = cudaMallocPitch(&dB.ptr, &dB.pitch, k * sizeof(float), n);
+	err = cudaMallocPitch(&dB.ptr, &dB.pitch, k * sizeof(float), m);
 	printf("[MatMul]: cudaMallocPitch B - %s\n", cudaGetErrorString(err));
-	err = cudaMallocPitch(&dC.ptr, &dC.pitch, k * sizeof(float), m);
+	err = cudaMallocPitch(&dC.ptr, &dC.pitch, k * sizeof(float), n);
 	printf("[MatMul]: cudaMallocPitch C - %s\n", cudaGetErrorString(err));
 	/* ------------------------- */
 
@@ -174,16 +254,16 @@ void MatMul(cudaPitchedPtr A, cudaPitchedPtr B, cudaPitchedPtr C, int m, int n, 
 	// info(dC);
 
 	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-	int bw = (m - 1) / BLOCK_SIZE + 1;
-	int bh = (m - 1) / BLOCK_SIZE + 1;
-	//dim3 dimGrid(k / BLOCK_SIZE, m / BLOCK_SIZE);
+	int bw = (n - 1) / BLOCK_SIZE + 1;
+	int bh = (n - 1) / BLOCK_SIZE + 1;
+	//dim3 dimGrid(k / BLOCK_SIZE, n / BLOCK_SIZE);
 	dim3 dimGrid(bw, bh);
 
 	/* Launch the kernel      */
 	cudaEventRecord(start, 0);
-		MatMulKernelShared <<< dimGrid, dimBlock >>> ((float *) dA.ptr,(float *) dB.ptr,(float *) dC.ptr, m, n, k);
+		MatMulKernelShared <<< dimGrid, dimBlock >>> ((float *) dA.ptr,(float *) dB.ptr,(float *) dC.ptr, n, m, k);
 		//MatMulKernel <<< dimGrid, dimBlock >>> (dA, dB, dC);
-		//kernel_shared <<< dimGrid, dimBlock >>> (dA, dB, dC, m, n, k, BLOCK_SIZE);
+		//kernel_shared <<< dimGrid, dimBlock >>> (dA, dB, dC, n, m, k, BLOCK_SIZE);
 
 	err = cudaThreadSynchronize(); cudaEventRecord(stop, 0); cudaEventSynchronize(stop); cudaEventElapsedTime(&timer, start, stop);
 	printf("[MatMul]: Run kernel - %s (%f ms)\n", cudaGetErrorString(err), timer);
@@ -259,17 +339,17 @@ int main(int argc, char ** argv)
 {
 	srand(time(NULL));
 
-	int m, n, k;
-	sscanf(argv[1], "%d", &m);
-	sscanf(argv[2], "%d", &n);
+	int n, m, k;
+	sscanf(argv[1], "%d", &n);
+	sscanf(argv[2], "%d", &m);
 	sscanf(argv[3], "%d", &k);
 
 	cudaPitchedPtr A, B, C, hC;
 
-	A = make_cudaPitchedPtr(malloc(m * n * sizeof(float)), n * sizeof(float), n, m);
-	B = make_cudaPitchedPtr(malloc(n * k * sizeof(float)), k * sizeof(float), k, n);
-	C = make_cudaPitchedPtr(malloc(m * k * sizeof(float)), k * sizeof(float), k, m);
-	hC = make_cudaPitchedPtr(malloc(m * k * sizeof(float)), k * sizeof(float), k, m);
+	A = make_cudaPitchedPtr(malloc(n * m * sizeof(float)), m * sizeof(float), m, n);
+	B = make_cudaPitchedPtr(malloc(m * k * sizeof(float)), k * sizeof(float), k, m);
+	C = make_cudaPitchedPtr(malloc(n * k * sizeof(float)), k * sizeof(float), k, n);
+	hC = make_cudaPitchedPtr(malloc(n * k * sizeof(float)), k * sizeof(float), k, n);
 	
 	// info(A);
 	// info(B);
@@ -280,11 +360,12 @@ int main(int argc, char ** argv)
 	make_random(C);
 
 	hostmul(A, B, hC);
-	MatMul(A, B, C, m, n, k);
+	//show(hC);
+	launch_kernel_shared(A, B, C, n, m, k, BLOCK_SIZE_H);
 
 	check(C, hC, 0.0001);
 
 	// show(A);
 	// show(B);
-	// show(C);
+	//show(C);
 }
